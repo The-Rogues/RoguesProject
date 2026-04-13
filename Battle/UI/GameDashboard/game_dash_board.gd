21b820931440d3b36cb0806285a067ed09f4552c
itzlesho <102629685+itzlesho@users.noreply.github.com>
2026-04-13 11:47:32 -0700

    Added global audio
    Works for all scenes rn but will stop automatically when battle scene comes up

--- Battle/UI/GameDashboard/game_dash_board.gd
+++ Battle/UI/GameDashboard/game_dash_board.gd
@@ -10,7 +10,7 @@ class_name GameSessionDisplay
 @onready var player_items: PlayerItems = $Container/RightElements/PlayerItems
 
 @onready var deck_button: TextureButton = $Container/RightElements/Deck/DeckButton
-
+@onready var options_menu = OptionsMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Menus/OptionsMenu/options_menu.gd
+++ Menus/OptionsMenu/options_menu.gd
@@ -1,5 +1,5 @@
 # Author: Nathaniel
-# Editor: Fabian
+# Editor: Fabian, Andy 
 
 # Will be used to update an options resource which will configure
 # different technical elements of the game
@@ -14,6 +14,7 @@ signal close
 @onready var master_volume: HSlider = $OptionsMenuElements/MarginContainer/TabContainer/Audio/VBoxContainer/HBoxContainer/MasterVolume
 @onready var configuration: LineEdit = $OptionsMenuElements/MarginContainer/TabContainer/Controls/VBoxContainer/HBoxContainer/Configuration
 
+
 func _on_go_back_button_up() -> void:
 	visible = false
 	close.emit()
@@ -21,3 +22,27 @@ func _on_go_back_button_up() -> void:
 
 # TODO: Connect signals for each settings uption to a function that updates a
 # options resource, which will be used to configure systems
+
+
+#When main menu button is pressed the scene tree changes to the main menu scene and deletes the current one. 
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/MainMenu/main_menu_scene.tscn")
+
+
+#As the volumne slide is changed it affects the music bus 
+func _on_music_slider_value_changed(value: float) -> void:
+	MusicManager.set_music_volume(value)
+	
+	#Change the value of the music bus 
+
+#How does this work? There is no literal signal here rn? 
+func _on_settings_pressed() -> void:
+	visible = true
+  
+
+func _on_master_volume_value_changed(value: float) -> void:
+	MusicManager.set_master_volume(value)
+
+
+func _on_music_on_off_toggled(toggled_on: bool) -> void:
+	MusicManager.set_music_enabled(toggled_on)
